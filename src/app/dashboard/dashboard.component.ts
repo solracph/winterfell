3e6cda90dea0fad0b3e77232524288290c3ec3f5
solracphcu <solracphcu@gmail.com>
2018-02-09 08:29:10 -0500

    0209201800

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -14,92 +14,139 @@ import { forEach } from '@angular/router/src/utils/collection';
 
 export class DashboardComponent implements OnInit {
     
-  onItemDrop(e: any) {
-    this.dashboard.push(e.dragData)
-
-    for(var item in this.dashboard){
-      
-    }
+ 
+  seriesClick(e: any){
+    console.log(e.dataItem.id)
   }
 
-  public events: string[] = [];
-  public series: any[] = [{
-          name: 'India',
-          type: 'line',
-          data: [3.907, 7.943, 7.848, 9.284, 9.263, 9.801, 3.890, 8.238, 9.552, 6.855]
-      }, {
-          name: 'Russian Federation',
-          data: [4.743, 7.295, 7.175, 6.376, 8.153, 8.535, 5.247, -7.832, 4.3, 4.3]
-      }, {
-          name: 'Germany',
-          type: 'line',
-          data: [0.010, -0.375, 1.161, 0.684, 3.7, 3.269, 1.083, -5.127, 3.690, 2.995]
-      }, {
-          name: 'World',
-          type: 'bar',
-          data: [1.988, 2.733, 3.994, 3.464, 4.001, 3.939, 1.333, -2.245, 4.339, 2.727]
-      }];
-
-    public series2: any[] = [{
-        name: 'India',
-        type: 'line',
-        data: [3.907, 7.943, 7.848, 9.284, 9.263, 9.801, 3.890, 8.238, 9.552, 6.855]
-    }, {
-        name: 'Russian Federation',
-        data: [4.743, 7.295, 7.175, 6.376, 8.153, 8.535, 5.247, -7.832, 4.3, 4.3]
-    }, {
-        name: 'Germany',
-        type: 'line',
-        data: [0.010, -0.375, 1.161, 0.684, 3.7, 3.269, 1.083, -5.127, 3.690, 2.995]
-    }, {
-        name: 'World',
-        type: 'bar',
-        data: [1.988, 2.733, 3.994, 3.464, 4.001, 3.939, 1.333, -2.245, 4.339, 2.727]
-    }];
-
-  public categories: number[] = [2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011];
   
-  public dashboard = [{
-        id:1,
-        name: 'India',
-        col: [{ id:1, name: "Col 1" },{id:2, name: "Col 2" },{id:3, name: "Col 3" }]
-    }, {
-        id:2,
-        name: 'Russian Federation',
-        col: [{id:4, name: "Col 4" },{id:5, name: "Col 5" },{id:6, name: "Col 6" }]
-    }, {
-        id:3,
-        name: 'Germany',
-        col: [{id:7, name: "Col 7" }]
-    }, {
-        id:4,
-        name: 'World',
-        col: [{id:8, name: "Col 8" }]
-    }];
 
+  public events: string[] = [];
+  public series2;
+  public series: Series = 
+    {
+        reports:
+        [{
+            id: 1,
+            serie:
+                [{
+                    name: 'Center',
+                    color: '#689f38',
+                    type: 'line',
+                    data: [{id:1, value: 1.3164}, {id:2, value: 98.0392}, {id:3, value: 63.6892},{id:4, value: 66.9572} , {id:5, value: 56.8627},{id:6, value: 3.939} ]
+                },{
+                    name: 'Mean',
+                    color: '#0288d1',
+                    type: 'bar',
+                    data: [{id:1, value: 57.4047}, {id:2, value: 56.8627}, {id:3, value: 3.994},{id:4, value: 3.464} , {id:5, value: 4.001},{id:6, value: 3.939} ]
+                }, {
+                    name: 'Goal',
+                    color: '#fbc02d',
+                    type: 'bar',
+                    data: [{id:1, value: 1.0000}, {id:2, value: 1.0000}, {id:3, value: 1.0000},{id:4, value: 1.0000} , {id:5, value: 1.0000},{id:6, value: 1.0000} ]
+                }]
+        }
+        ,{
+            id: 2,
+            serie:
+                [{
+                    name: 'Center',
+                    color: '#689f38',
+                    type: 'line',
+                    data: [{id:1, value: 1.3164}, {id:2, value: 98.0392}, {id:3, value: 53.6892},{id:4, value: 67.9572} , {id:5, value: 59.8627},{id:6, value: 3.939} ]
+                },{
+                    name: 'Mean',
+                    color: '#0288d1',
+                    type: 'line',
+                    data: [{id:1, value: 36.4047}, {id:2, value: 56.8627}, {id:3, value: 5.994},{id:4, value: 2.464} , {id:5, value: 4.001},{id:6, value: 3.939} ]
+                }, {
+                    name: 'Goal',
+                    color: '#fbc02d',
+                    type: 'bar',
+                    data: [{id:1, value: 1.0000}, {id:2, value: 4.0000}, {id:3, value: 1.0000},{id:4, value: 6.0000} , {id:5, value: 1.0000},{id:6, value: 1.0000} ]
+                }]
+        }]
+    }
+
+    public valueAxis: any = {
+        labels: {
+            format: '#.00'
+        }
+    };
+
+    public categories: string[] = ["HC5.1", "HC6.1", "HC7.1", "HC7.2", "HC8.1", "HC8.2"];
+  
     dragOperation: boolean = false;
 
     containers: Array<Container> = [
-        new Container(1, 'Container 1', [new Widget('1','6'), new Widget('2','6')]),
-        new Container(2, 'Container 2', [new Widget('3','6'), new Widget('4','6')]),
-        new Container(3, 'Container 3', [new Widget('5','6'), new Widget('6','6')])
+        new Container(1, 'Container 1', [new Widget(1,'1','6',[]) ,new Widget(2,'2','6',[])])
+       /* new Container(2, 'Container 2', [new Widget(3,'3','4'), new Widget(4,'4','6')]),
+        new Container(3, 'Container 3', [new Widget(5,'5','6'), new Widget(6,'6','6')])*/
     ];
 
   items: Observable<AngularFireAction<any>[]>;
 
-  constructor(private service: DashboardService){
-    this.items = this.service.getItems();
-  }
+    getwidgetSeries(id:number): Promise<any>{
+        var serie;
+
+        this.series.reports.forEach(
+            report =>{
+                if(report.id == id)
+                {
+                    serie = report.serie;
+                }
+            }
+        );
+
+        return Promise.resolve(serie);
+    }
+
+    constructor(private service: DashboardService){
+        
+    }
 
   ngOnInit() {
+    this.containers.forEach(
+        container => {
+            container.widgets.forEach(
+                widget => {
+                    this.getwidgetSeries(widget.id).then(data => {
+                        widget.data = data;
+                    })
+                }
+            )
+        }
+     )
   }
 }
 
+interface Series {
+    reports: Array<WidgetReport>
+}
+
+interface WidgetReport {
+    id : number,
+    serie : Array<SerieConfiguration>
+}
+
+interface SerieConfiguration{
+    name: string,
+    color: string,
+    type: string,
+    data: Array<SerieData>
+}
+
+interface SerieData{
+    id:number, 
+    value: number
+}
 
 class Container {
   constructor(public id: number, public name: string, public widgets: Array<Widget>) {}
 }
 
 class Widget {
-  constructor(public name: string, public width: string) {}
+  constructor(public id: number,public name: string, public width: string, public data: any) {
+    
+  }
 }
